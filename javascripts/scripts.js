17672741a9c701f11f8f60ee75af84689e8800d3
Jehan Tremback <jehan.tremback@gmail.com>
2012-06-21 23:35:58 -0700

    Completed forms and added styling

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   // Bind an event to window.onhashchange that, when the hash changes, gets the
   // hash and adds the class "selected" to any matching nav link.
   $(window).hashchange( function(){
-    var hash = location.hash || '#login2';
+    var hash = location.hash || '#login1';
 
 //    // Set the page title based on the hash.
 //    document.title = ( hash.replace( /^#/, '' ) || 'blank' ) + ' | Hypothes.is';
@@ -15,5 +15,5 @@ $(document).ready(function() {
     $('#navlist a[href="' + hash + '"]').addClass("hover")
   });
   // Hide the invisible things ASAP on page load.
-  $('.page').not(location.hash || '#login2').hide()
+  $('.page').not(location.hash || '#login1').hide()
 });
